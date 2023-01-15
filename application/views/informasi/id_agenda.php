93557f9198f1180638ffa864627c2f1ba5838074
Nadhifa <nadhifartf27@gmail.com>
2023-01-15 23:41:15 +0800

    nadip15
    update terbaru

--- application/views/informasi/id_agenda.php
+++
@@ -1,8 +0,0 @@
-<section class="page-section">
-    <div class="container p-5">
-<h1>
-	<?= $agenda['0']['isi_agenda']?>
-</h1>
-	</div>
-</body>
-</html>
--- application/views/template/footer.php
+++
@@ -1,8 +0,0 @@
-<script src="<?=base_url();?>assets/js/scripts.js"></script>
-<script src="<?= base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?= base_url(); ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-</body>
-
-</html>
